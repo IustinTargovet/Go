4cdbdf7c0a4b5c17ca75422048b233899b82cd7c
Iustin Targovet <j_tirgov@live.com>
2018-06-11 16:05:37 +0100

    full testing finished

--- Cards/deck_test.go
+++ Cards/deck_test.go
@@ -1,10 +1,33 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 	if len(d) != 52 {
 		t.Errorf("Expeted deck length of 52, but got %v", len(d))
 	}
+
+	if d[0] != "A of Spades" {
+		t.Errorf("Expected first value A of Spades but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "K of Clubs" {
+		t.Errorf("Expected first value K of Clubs but got %v", d[0])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+	if len(loadedDeck) != 52 {
+		t.Errorf("exppected 52 got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }

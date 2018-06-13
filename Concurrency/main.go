dc86b6202c6c68797945572ad68a99cb554026ee
Iustin Targovet <j_tirgov@live.com>
2018-06-13 14:36:25 +0100

    infinite loop

--- Concurrency/main.go
+++ Concurrency/main.go
@@ -9,11 +9,11 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 	fmt.Println(link, "is up!")
-	c <- "Yup, it's up"
+	c <- link
 }
 
 func main() {
@@ -29,7 +29,9 @@ func main() {
 
 	for _, link := range links {
 		go checkLink(link, c)
-		fmt.Println(<-c)
 	}
 
+	for {
+		go checkLink(<-c, c)
+	}
 }
